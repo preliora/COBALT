378437fa0ff1b4571a5e30d906e07dc07be47566
preliora <81929697+preliora@users.noreply.github.com>
2023-04-29 19:40:50 -0700

    main

---
+++ Untitled+.py
@@ -0,0 +1,18 @@
+def ifItem(first, second):
+    if first == second:
+        print(True)
+def version():
+    print("This is Trollge V1.0")
+def getValue(value, getSpecificValueInList, specificValue, pos):
+    
+    if value.find('[') == -1:
+        print(value.split(':')[1])
+    else:
+        print(value.split('[')[1])
+        print(value.split(':')[0] + ' is a list')
+    
+    if getSpecificValueInList == True:
+        print(value.split('[')[1].split(specificValue)[pos])
+
+
+getValue('testing:[thing:buddy]', False, False, False)
